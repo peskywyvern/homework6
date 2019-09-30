2c3b5fc1fbb043960ad4992fb384ce0fe960508f
Natalie D <55387925+peskywyvern@users.noreply.github.com>
2019-09-30 23:32:37 +0300

    Add files via upload

--- 61.py
+++ 61.py
@@ -1,11 +1,10 @@
 def insert_whitespace(text):
     new_text = ''
     for character in text:
-        index = text.index(character)
-        if index != len(text) - 1 and character.islower() and text[index+1].isupper():
-            new_text = f'{new_text}{character} '
+        if character.isupper():
+            new_text += ' ' + character
         else:
-            new_text = f'{new_text}{character}'
+            new_text += character
     return new_text
 
 
--- 63.py
+++ 63.py
@@ -1,10 +1,10 @@
 def wrap(string, width):
     new_string = ''
-    for character in string:
-        if string.index(character) % width == 0:
+    for index, character in enumerate(string):
+        if index % width == 0:
             new_string = f'{new_string} \n'
         new_string = f'{new_string}{character}'
     return new_string
 
 
-print(wrap('ABCDEFGHIJKLMNOP', 5))
+print(wrap('testestestest', 2))
